b9b70a02c26beba4111bf15852ebe82aa9c46648
Raja L <rajal@Raja-3.local>
2024-02-24 20:01:58 +0530

    spring samples project

---
+++ spring-framework-example/src/main/java/com/raddish/spring_framework_example/App.java
@@ -0,0 +1,10 @@
+package com.raddish.spring_framework_example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
